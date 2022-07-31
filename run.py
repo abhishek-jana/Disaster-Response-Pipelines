a38c761824c31f87a4583ac622b25959a41027ca
Abhishek-jana <iiserabhishek@gmail.com>
2022-07-31 03:27:31 -0500

    modified run file

--- run.py
+++ run.py
@@ -169,16 +169,16 @@ def go():
 
 
 port = int(os.getenv("PORT", 5000))
-# if __name__ == "__main__":
-#     host = '0.0.0.0'
-#     # port = 5000
-#     httpd = simple_server.make_server(host, port, app)
-#     # print("Serving on %s %d" % (host, port))
-#     httpd.serve_forever()
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    # port = 5000
+    httpd = simple_server.make_server(host, port, app)
+    # print("Serving on %s %d" % (host, port))
+    httpd.serve_forever()
 
-def main():
-    app.run(host='0.0.0.0', port=port, debug=True)
-
-
-if __name__ == '__main__':
-    main()
+# def main():
+#     app.run(host='0.0.0.0', port=port, debug=True)
+#
+#
+# if __name__ == '__main__':
+#     main()
